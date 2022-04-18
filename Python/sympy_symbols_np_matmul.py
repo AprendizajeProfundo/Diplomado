482e9c9e6dd29cdc5f63caece283b3e410f8f7bc
FM <fedom9@gmail.com>
2022-04-18 03:35:45 -0500

    Update sympy_symbols_np_matmul.py

--- Python/sympy_symbols_np_matmul.py
+++ Python/sympy_symbols_np_matmul.py
@@ -16,3 +16,16 @@ n = sympy.symbols("n")
 M = (I-(1/n)*np.matmul(Ones,Onest))
 S = (1/n)*np.matmul(np.matmul(Xt,M), X)
 S[0,0]
+
+X = MatrixSymbol('X', 1, 2)
+b = np.array((0,0))
+A = np.matrix([[0.5/0.72,-0.8/0.72],[-0.8/0.72,2/0.72]])
+
+X = MatrixSymbol('X', 1, 2)
+b = np.array((4,3))
+A = np.matrix([[2,1],[1,1]])
+
+np.matmul(A, np.transpose(X-b))
+r = np.matmul(np.matmul((X-b), A), np.transpose(X-b))
+
+np.sqrt(np.linalg.det(A))
